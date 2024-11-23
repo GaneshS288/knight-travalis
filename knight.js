4187af50f9ceff4f58b0c1ffc8bf0ffaa1bfdc88
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2024-11-23 21:58:52 +0530

    complete knight travalis

--- knight.js
+++ knight.js
@@ -6,7 +6,7 @@ class Node {
   }
 }
 
-function getAllMoves(x, y) {
+function getAllMoves(x, y, end) {
   let moveArray = [
     [x - 1, y - 2],
     [x - 1, y + 2],
@@ -23,11 +23,59 @@ function getAllMoves(x, y) {
     else return true;
   });
 
+  moveArray = moveArray.filter((item) => {
+    if (Math.abs(x - end[0]) <= 1 && Math.abs(y - end[1]) <= 1) return true;
+    if (
+      Math.abs(x - end[0]) > Math.abs(item[0] - end[0]) ||
+      Math.abs(y - end[1]) > Math.abs(item[1] - end[1])
+    )
+      return true;
+    else return false;
+  });
+
   return moveArray;
 }
 
+let count = 0;
+
 function knightMoves(start, end) {
-  return;
+  let adjacencyList = [];
+  let queue = [new Node(start)];
+
+  //if the first element in queue matches end coordinates then break the loop cause we reached our destination
+  while (
+    queue[0].coordinates[0] !== end[0] ||
+    queue[0].coordinates[1] !== end[1]
+  ) {
+    console.log(queue[0].coordinates);
+    count++;
+    let currentPosition = queue.shift();
+
+    //get all possible moves of current position and then turn them into nodes with their previous property set to current position node
+    for (let coordinates of getAllMoves(
+      currentPosition.coordinates[0],
+      currentPosition.coordinates[1],
+      end
+    )) {
+      queue.push(new Node(coordinates, currentPosition));
+    }
+
+    //push the current position node into list so it doesn't get garbage collected
+    adjacencyList.push(currentPosition);
+  }
+
+  let path = [];
+  let currentPosition = queue[0];
+
+  //get path by traversing the previous property of nodes
+  while (true) {
+    path.push(currentPosition.coordinates);
+    if (currentPosition.previous === null) break;
+    else currentPosition = currentPosition.previous;
+  }
+
+  return path.reverse();
 }
 
-console.log(getAllMoves(2, 1));
+console.log(knightMoves([3, 3], [3, 2]));
+console.log(count);
